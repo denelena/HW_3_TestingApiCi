ba8a5f89ea5d3f43cfd6bd347c26379b03868611
Elena Denisova <lenaden@gmail.com>
2022-05-04 17:01:20 -0400

    initial commit

---
+++ src/test/java/ru/netology/tests/PostmanEchoTest.java
@@ -0,0 +1,32 @@
+package ru.netology.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostmanEchoTest {
+
+    private String payLoad =
+                    "Twas brillig, and the slithy toves"+
+                    "Did gyre and gimble in the wabe;"+
+                    "All mimsy were the borogoves,"+
+                    "And the mome raths outgrabe.";
+
+
+    @Test
+    void shouldGetResponseFromPostmanEcho() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(payLoad) // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(payLoad));
+    }
+}
